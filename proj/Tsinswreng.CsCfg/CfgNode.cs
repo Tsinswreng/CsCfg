34e0874c0bbecce1a3fba22c9b8b06e43d268801
Tsinswreng <Tsinswreng@gmail.com>
2026-04-04 14:36:37 +0800

    DfltValue從DfltValueObj轉發

--- proj/Tsinswreng.CsCfg/CfgNode.cs
+++ proj/Tsinswreng.CsCfg/CfgNode.cs
@@ -11,24 +11,13 @@ public partial class CfgNode<T>:ICfgNode<T>{
 	public CfgNode(){}
 	public IList<str> RelaPathSegs{get;set;} = [];
 	public str? _FullPathCache{get;set;}
-	public T? DfltValue{get;set;}
-	public obj? DfltValueObj{
-		get=>DfltValue;
-		set=>DfltValue = CastToT(value);
+	public obj? DfltValueObj{get;set;}
+	public T? DfltValue{
+		get=>DfltValueObj is null?default:(T?)DfltValueObj;
+		set=>DfltValueObj = value;
 	}
 	public ICfgNode? Parent{get;set;}
 	public IList<ICfgNode>? Children{get;set;}
-
-	static T? CastToT(obj? Value){
-		if(Value is null){
-			return default;
-		}
-		if(Value is T Typed){
-			return Typed;
-		}
-		var TargetType = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
-		return (T)Convert.ChangeType(Value, TargetType);
-	}
 
 	public static ICfgNode<object?> Mk(
 		ICfgNode? Parent
